394bbc88ed47000bbb4a72fcd877864b30119e92
indiegeeker <indiegeeker@gmail.com>
2025-08-14 16:50:54 +0800

    fix: app core export

--- packages/app_core/lib/app_core.dart
+++ packages/app_core/lib/app_core.dart
@@ -1,6 +1,13 @@
 export 'src/app_manager.dart';
 export 'src/app_config.dart';
-
+/// 通用接口
 export 'package:app_interfaces/app_interfaces.dart' show ResponseParser, ParsedResult, ApiResponse;
+
+/// 存储
+export 'package:app_storage/app_storage.dart' show SharedPrefsStorage, StorageConfig;
+
+/// 网络
+export 'package:app_network/app_network.dart' show NetworkClientFactory, NetworkConfig,NetworkClient;
+
 
 export 'package:app_logger/app_logger.dart' show Logger;
